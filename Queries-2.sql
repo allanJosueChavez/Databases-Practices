59745dc6ed2cae58c95ee6f8e5590f320b2ba38d
Allan Chávez <allancss2009@hotmail.com>
2022-10-12 13:49:53 -0600

    feat: queries part two

---
+++ Queries-2.sql
@@ -0,0 +1,11 @@
+--Insert
+INSERT INTO teams (name, created_at, updated_at) VALUES ('Guatemala', now(), now());
+--Update
+UPDATE teams SET name = 'Colombia' WHERE id = 8;
+--Delete
+DELETE FROM table_name WHERE condition;
+--Unions
+--Having
+--Joins
+--Exists
+--Like
