36941cb1a07f103560aeb9f613176a63beda59db
Allan Chávez <allancss2009@hotmail.com>
2022-10-12 20:03:11 -0600

    feat: joins explanations and queries

--- Queries-2.sql
+++ Queries-2.sql
@@ -1,11 +1,43 @@
 --Insert
 INSERT INTO teams (name, created_at, updated_at) VALUES ('Guatemala', now(), now());
+INSERT INTO teams (name, created_at, updated_at) VALUES ('Costa Rica', now(), now());
+
 --Update
-UPDATE teams SET name = 'Colombia' WHERE id = 8;
+UPDATE teams SET name = 'Colombia FC' WHERE name = "Colombia";
+UPDATE matches SET goals_first_team = 3, goals_second_team = 0 WHERE id = 1;
+
 --Delete
-DELETE FROM table_name WHERE condition;
+DELETE FROM teams WHERE name = 'Guatemala';
+DELETE FROM cards WHERE users_id = 1; --This is taking all cards of the user?
+
 --Unions
+SELECT name FROM users
+UNION
+SELECT email FROM users;
+
 --Having
+SELECT COUNT(successful), (SELECT name, id from users where id = users_id)
+FROM predictions
+GROUP BY successful
+HAVING COUNT(matches_id) > 5;
+
 --Joins
+SELECT predictions.id, team_a_winner, team_b_winner, match_tied, users.name, predictions.created_at
+FROM predictions
+INNER JOIN users
+ON predictions.users_id = users.id;
+
 --Exists
+SELECT name
+FROM matches
+WHERE EXISTS
+(SELECT name FROM teams WHERE name = 'Guatemala');
+
 --Like
+SELECT * FROM users
+WHERE email LIKE '%gmail.com';
+SELECT * FROM matches
+WHERE name LIKE '%Brazil';
+
+--What is the difference between INNER and OUTER JOIN?
+--What is the difference between RIGHT and LEFT JOIN?
